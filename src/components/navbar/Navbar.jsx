bd0393091ce5862a0c0e0a57083badbe77fb796e
Tihara Egodage <tiharaeg@gmail.com>
2024-03-30 13:02:36 +0530

    update: main component files

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="Facebook" width={24} height={24} />
+        <Image src="/instagram.png" alt="Facebook" width={24} height={24} />
+        <Image src="/tiktok.png" alt="Facebook" width={24} height={24} />
+        <Image src="/youtube.png" alt="Facebook" width={24} height={24} />
+      </div>
+      <div className={styles.logo}></div>
+      <div className={styles.links}></div>
+    </div>
+  );
+};
+
+export default Navbar;
