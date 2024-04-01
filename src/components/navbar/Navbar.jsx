2df845f18af185bde8e16285a31cc851614b392f
Tihara Egodage <tiharaeg@gmail.com>
2024-04-01 10:46:29 +0530

    update: progress

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
+import ThemeToggle from "../themeToggle/ThemeToggle";
+import AuthLinks from "../authLinks/AuthLinks";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -11,8 +14,14 @@ const Navbar = () => {
         <Image src="/tiktok.png" alt="Facebook" width={24} height={24} />
         <Image src="/youtube.png" alt="Facebook" width={24} height={24} />
       </div>
-      <div className={styles.logo}></div>
-      <div className={styles.links}></div>
+      <div className={styles.logo}>Tee Blog</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <AuthLinks />
+      </div>
     </div>
   );
 };
